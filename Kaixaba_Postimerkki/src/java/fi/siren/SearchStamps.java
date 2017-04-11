d12bf388942d59fdaf9b9cbaa10e38fcd28f135f
Eerik <eerik.timonen94@gmail.com>
2017-04-11 19:52:41 +0300

    -Delete couple of lines
    -Delete couple of lines from SearchStamps.java

--- Kaixaba_Postimerkki/src/java/fi/siren/SearchStamps.java
+++ Kaixaba_Postimerkki/src/java/fi/siren/SearchStamps.java
@@ -32,18 +32,7 @@ public class SearchStamps extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SearchStamps</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SearchStamps at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+       
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
