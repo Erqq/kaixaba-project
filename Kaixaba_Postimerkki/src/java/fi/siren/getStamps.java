c5af97fbba3d855f45b59d03045e3c5fcb310d06
Eerik <eerik.timonen94@gmail.com>
2017-04-18 18:59:46 +0300

    Add search option
    Add search option to the frontpage

--- Kaixaba_Postimerkki/src/java/fi/siren/getStamps.java
+++ Kaixaba_Postimerkki/src/java/fi/siren/getStamps.java
@@ -1,9 +1,16 @@
 package fi.siren;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -43,6 +50,46 @@ public class getStamps extends HttpServlet {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+      @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    InputStream stream = new ByteArrayInputStream(request.getReader().readLine().getBytes(StandardCharsets.UTF_8));
+    JsonReader jsonReader = Json.createReader(stream);
+    JsonObject js = jsonReader.readObject();
+    jsonReader.close();
+    fixHeaders(response);
+    List<Stamp> stamps;
+    List<Stamp> temp=new ArrayList<>();
+        try (PrintWriter out = response.getWriter()) {
+            stamps = stmp.getStamps();
+            for(int i = 0; i< stamps.size();i++){
+                if(stamps.get(i).getTags().toLowerCase().replace(" ","")
+                        .contains(js.getString("tags").toLowerCase()
+                                .replace(" ",""))){
+                    temp.add(stamps.get(i));
+                }   
+             }
+            out.println("[");
+             
+            for(int i = 0; i< temp.size();i++){
+                    
+                   
+                    out.println(temp.get(i).toJson());
+                    if (i +1< temp.size()) {
+                    out.println(", "); 
+                    }
+               
+                  
+             }
+            out.println("]");    
+              
+            out.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
     }
     
     
