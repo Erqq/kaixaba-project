e9b0e968929333b12f07884ac89601cef8963661
Eerik <eerik.timonen94@gmail.com>
2017-04-21 20:25:50 +0300

    Add comments.
    Add comments.

--- Kaixaba_Postimerkki/src/java/fi/siren/StampBean.java
+++ Kaixaba_Postimerkki/src/java/fi/siren/StampBean.java
@@ -7,7 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
  
 /**
- *
+ * Enables interaction with the database.
  * @author Miika
  */
 @Stateless
@@ -15,20 +15,41 @@ public class StampBean implements StampService {
      
     @PersistenceContext(unitName = "postimerkit")
     private EntityManager em;
- 
+    
+    /**
+     * Creates a new stamp to the database.
+     * @param stamp
+     * @return stamp
+     */
     @Override
     public Stamp createOrUpdate(Stamp stamp) {
         return em.merge(stamp);
     }
+    
+    /**
+     * Removes stamp from the database.
+     * @param stamp 
+     */
     @Override
     public void remove(Stamp stamp) {
         em.remove(em.merge(stamp));
     }
+    
+    /**
+     * Returns stamp from the database which id is given as parameter.
+     * @param id
+     * @return stamp.
+     */
     @Override
     public Stamp find(Object id) {
         return em.find(fi.siren.Stamp.class, id);
     }
      
+    /**
+     * Gets all stamps from the database.
+     * @return stamps
+     * @throws Exception 
+     */
     @Override
     public List<Stamp> getStamps() throws Exception {
         Query query = em.createQuery("Select obj from Stamp as obj");
--- Kaixaba_Postimerkki/src/java/fi/siren/StampService.java
+++ Kaixaba_Postimerkki/src/java/fi/siren/StampService.java
@@ -4,13 +4,36 @@ import java.util.List;
 import javax.ejb.Local;
  
 /**
- *
+ * Enables interaction with the database.
  * @author Miika
  */
 @Local
 public interface StampService {
+    
+   /**
+     * Creates a new stamp to the database.
+     * @param stamp
+     * @return stamp
+     */
     Stamp createOrUpdate(Stamp stamp);
+    
+    /**
+     * Removes stamp from the database.
+     * @param stamp 
+     */
     void remove(Stamp stamp);
+    
+    /**
+     * Returns stamp from the database which id is given as parameter.
+     * @param id
+     * @return stamp.
+     */
     Stamp find(Object id);
+    
+    /**
+     * Gets all stamps from the database.
+     * @return stamps
+     * @throws Exception 
+     */
     List<Stamp> getStamps() throws Exception;
 }
